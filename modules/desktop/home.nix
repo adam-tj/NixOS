9320d82e9c1c1f9aebb50dbf96c8a774eb422ed1
adam-tj <adamplusplus@gmail.com>
2025-03-31 22:02:55 +0200

    updated home.nix

--- modules/desktop/home.nix
+++ modules/desktop/home.nix
@@ -39,31 +39,10 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
 
-#     appimage-run
-#     bottles
-#     discord
-#     # flameshot
-#     gearlever gimp google-chrome
-#     irssi isoimagewriter
-# #     glxinfo
-#     jdk jellyfin-media-player jellyfin-mpv-shim jetbrains-toolbox jflap
-#     kdePackages.filelight kdePackages.kate kdePackages.kolourpaint kdePackages.konversation  # kdePackages.kalk kodi
-#     libreoffice-fresh # lutris
-#     mangohud mediainfo mesa-demos # mesen
-# #     nix-janitor
-#      (mpv-unwrapped.wrapper { mpv = mpv-unwrapped.override { vapoursynthSupport = true; }; youtubeSupport = true; })
-#     obs-studio
-#     piper plex-desktop plex-mpv-shim protonplus protontricks # protonup-qt
-#     qalculate-qt quasselClient qbittorrent # quadrapassel
-#     r2modman remmina
-#     /* sameboy */ skypeforlinux smplayer svp #     slippi-netplay
-#     vlc
-#     wine
-#     zapzap zoom-us
-# #     (nixgl.nixGLNvidia.override { inherit (pkgs) mesa; }).wrapApp
-# #     nixgl.auto.nixGLNvidia
-# #     pkgs-unstable.umu-launcher
-# #      pkgs-unstable.kdePackages.krdc
+#     (nixgl.nixGLNvidia.override { inherit (pkgs) mesa; }).wrapApp
+#     nixgl.auto.nixGLNvidia
+#     pkgs-unstable.umu-launcher
+#      pkgs-unstable.kdePackages.krdc
 
   ];
 
