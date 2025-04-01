326d6e64630456c9537207817a6cf2c61d5eb402
adam-tj <adamplusplus@gmail.com>
2025-04-01 22:44:35 +0200

    hyprland fonts

--- modules/services/hyprland.nix
+++ modules/services/hyprland.nix
@@ -1,1 +1,16 @@
-{ programs.hyprland.enable = true; }
+{ pkgs, ... }:
+
+{
+  programs.hyprland.enable = true;
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    nerdfonts
+    roboto-mono
+    font-awesome
+  ];
+
+}
