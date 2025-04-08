affaddb067ef098f6346049b039117fd19f476ae
adam-tj <adamplusplus@gmail.com>
2025-04-08 04:57:11 +0200

    commented vscode

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-  inputs.nix-vscode-extensions = {
-    url = "github:nix-community/nix-vscode-extensions?ref=master";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
+  # inputs.nix-vscode-extensions = {
+  #   url = "github:nix-community/nix-vscode-extensions?ref=master";
+  #   inputs.nixpkgs.follows = "nixpkgs";
+  # };
   inputs.home-manager = {
     url = "github:nix-community/home-manager/release-24.11";
     inputs.nixpkgs.follows = "nixpkgs";
