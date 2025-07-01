a0102b335eabebfca91bdec84672cb2957bb1d78
adam-tj <adamplusplus@gmail.com>
2025-07-02 01:23:31 +0200

    nvidia stuff

--- modules/desktop/nvidia-gpu.nix
+++ modules/desktop/nvidia-gpu.nix
@@ -1,6 +1,46 @@
+# { config, ... }:
+# { 
+#   #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable; # Default
+#   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+# }
+
+# { config, lib, pkgs, ... }:
+# {
+
+#   # Enable OpenGL
+#   hardware.graphics = {
+#     enable = true;
+#     enable32Bit = true;
+#     extraPackages = with pkgs; [ nvidia-vaapi-driver egl-wayland ];
+#   };
+
+#   # Load nvidia driver for Xorg and Wayland
+#   services.xserver.videoDrivers = ["nvidia"];
+
+#   hardware.nvidia = {
+#     modesetting.enable = true;
+#     powerManagement.enable = true;
+#     powerManagement.finegrained = false;
+#     open = false;
+#     nvidiaSettings = true;
+#     package = config.boot.kernelPackages.nvidiaPackages.beta;
+#   };
+# }
+
+
+
+
+
+
+
+
+
+
+
+
 # Old stable config - requries kernel to be uncommented in boot.nix
 
-# { config, pkgs, ... }:
+#{ config, pkgs, ... }:
 
 # {
 #   hardware.graphics = {
@@ -20,7 +60,7 @@
 #   };
 # }
 
-# New DeepSeek config
+#New DeepSeek config
 
 { config, pkgs, inputs, ... }:
 let
@@ -32,7 +72,7 @@ let
 {
   nixpkgs.config.allowUnfree = true;
   # 1. First, ensure we're using the unstable kernel packages
-  boot.kernelPackages = unstable.linuxPackages_6_12;
+  boot.kernelPackages = unstable.linuxPackages_latest;
 
   # 2. Then configure NVIDIA to use the unstable driver
   hardware.nvidia = {
@@ -42,7 +82,7 @@ let
     nvidiaSettings = true;
     
     # This now points to the unstable version because we modified kernelPackages
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     
   };
 
