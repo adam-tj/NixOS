41cc5b1fa129e631681333b96ebd3404cdff2eac
adam-tj <adamplusplus@gmail.com>
2025-10-23 20:57:10 +0200

    updated desktop, added insecure package

--- modules/common/whitelist-insecure-packages.nix
+++ modules/common/whitelist-insecure-packages.nix
@@ -1,3 +1,7 @@
 {
-  nixpkgs.config.permittedInsecurePackages = [ "freeimage-3.18.0-unstable-2024-04-18" ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "freeimage-3.18.0-unstable-2024-04-18"
+    "mbedtls_2-2.28.10"
+    "mbedtls-2.28.10"
+  ];
 }
