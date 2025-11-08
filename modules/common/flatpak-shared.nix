bc181b140debc3e047dc6e7722f44d79bf001cf5
adam-tj <adamplusplus@gmail.com>
2025-11-08 03:14:29 +0100

    added openmw

--- modules/common/flatpak-shared.nix
+++ modules/common/flatpak-shared.nix
@@ -2,27 +2,25 @@
   "com.google.Chrome"
   "com.usebottles.bottles"
   "org.deluge_torrent.deluge"
-  # "com.discordapp.Discord"
-  "im.riot.Riot" # Element
+  "im.riot.Riot"                        # Element
   "org.mozilla.firefox"
   "it.mijorus.gearlever"
   "org.gimp.GIMP"
-  "io.github.milkshiift.GoofCord" # Third Party Discord Client
+  "io.github.milkshiift.GoofCord"       # Third Party Discord Client
   "com.google.EarthPro"
-  #"org.libreoffice.LibreOffice"
-  #"net.lutris.Lutris"
   "net.mediaarea.MediaInfo"
+  "org.openmw.OpenMW"
   "com.nextcloud.desktopclient.nextcloud"
-  "org.prismlauncher.PrismLauncher" # Minecraft
+  "org.prismlauncher.PrismLauncher"     # Minecraft Launcher
   "com.vysp3r.ProtonPlus"
   "com.github.Matoking.protontricks"
   "io.github.Qalculate.qalculate-qt"
   "org.quassel_irc.QuasselClient"
-  "com.github.ryonakano.reco" # Audio recorder
+  "com.github.ryonakano.reco"           # Audio recorder
   "org.remmina.Remmina"
   "org.torproject.torbrowser-launcher"
   "org.openscopeproject.TrguiNG"
-  "dev.vencord.Vesktop"  # Third Party Discord Client
+  "dev.vencord.Vesktop"                 # Third Party Discord Client
   "com.rtosta.zapzap"
   "us.zoom.Zoom"
 ]
