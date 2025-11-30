bd2e7a8d1bc424e4d707ce64645c2bc183a4cbde
adam-tj <adamplusplus@gmail.com>
2025-12-01 00:53:06 +0100

    boot splash attempts

--- modules/thinkpad/boot.nix
+++ modules/thinkpad/boot.nix
@@ -6,8 +6,13 @@
     plymouth = {
       enable = true;
       theme = "nixos-bgrt";
-      themePackages = with pkgsWithBgrt; [
-        nixos-bgrt-plymouth-no-firmware
+      themePackages = [
+        pkgsWithBgrt.nixos-bgrt-plymouth-no-firmware
+        pkgs.plymouth-matrix-theme
+        pkgs.plymouth-blahaj-theme
+        pkgs.kdePackages.breeze-plymouth
+        pkgs.catppuccin-plymouth
+        pkgs.adi1090x-plymouth-themes
       ];
       #logo = "${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake.png";
     };
