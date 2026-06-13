6d13ca00a83ba30346615b65ac2e937a40a5ae19
adam-tj <adamplusplus@gmail.com>
2026-06-13 04:31:03 +0200

    removed sshd from laptop

--- hosts/thinkpad.nix
+++ hosts/thinkpad.nix
@@ -43,18 +43,6 @@
 
   ];
 
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # For security, you can disable password login if you are strictly using SSH keys
-      PasswordAuthentication = true;
-
-      # Block root from logging in directly over SSH
-      PermitRootLogin = "no";
-    };
-  };
-
   networking.firewall.allowedTCPPorts = [ 22 ];
 
   nix.settings.trusted-users = [ "root" "adam" "@wheel" ];
