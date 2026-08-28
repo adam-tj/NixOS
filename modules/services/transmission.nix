964e1dd6031482d02ef5c17b55f3da1ad51ace7a
adam-tj <adamplusplus@gmail.com>
2026-08-29 01:45:40 +0200

    fixed transmission

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -3,7 +3,7 @@
 {
   services.transmission = {
     enable = true;
-    package = inputs.nixpkgs-transmission.legacyPackages.${pkgs.system}.transmission_4;
+    package = inputs.transmission.legacyPackages.${pkgs.system}.transmission_4;
 
     # Open firewall ports via native module options
     openRPCPort = true;   # Opens Web UI port (9091)
